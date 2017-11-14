079e293aa30f9c12f7b66cd34f1bd213db78d1f6
dima <dima012111@gmail.com>
2017-11-14 10:41:54 +0200

    test

--- src/main/java/test/Ierarhie.java
+++ src/main/java/test/Ierarhie.java
@@ -1,15 +1,22 @@
 package test;
+class Sup{
 
+}
+class Sub extends Sup{
+
+}
 public class Ierarhie {
     public static void main(String []s){
 
-//        Sup sup=new Sup();
-//        Sup sup1=new Sub();
-//        Sup sup2=sup;
-//        Sup sup3=sup1;
-//        Sub sub=new Sub();
-//        Sub sub1= (Sub) sup1;
-//        Sub sub2= (Sub) sup;
+        Sup sup=new Sup();
+        Sup sup1=new Sub();
+        Sup sup2=sup;
+        Sup sup3=sup1;
+        Sub sub=new Sub();
+        Sub sub1= (Sub) sup1;
+       Sup sup4=sub;
+
+
 
     }
 }
